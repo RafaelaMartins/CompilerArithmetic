24e21db9a94ada878921627919eadecb903a9e33
Rafa_Martins <rafaelamartinsv@gmail.com>
2019-12-01 16:52:35 -0300

    Ta foda essa vida

--- run.py
+++ run.py
@@ -1,29 +1,7 @@
-from tree import Node, Tree
 from stack import Stack
 
 if __name__ == "__main__":
     test = ["3","2","+","2","*"]
-    p = Stack()
-    for x in test:
-        if x.isdigit():
-            new = Node(x,None,None)
-            p.push(new)
-            p.leitura()
-        else:
-            node = Node(x)
-            node.r = p.pop()
-            node.l = p.pop()
-            print(type(node))
-            new = node
-            print(type(new))
-            #raiz.root = Node()
-            p.push(new)
-            p.leitura()
-    raiz = Tree()
-    raiz.root = p.pop()
-    print(raiz.root.v)
-    print(raiz.root.r.v)
-    print(raiz.root.l.v)
-    print(raiz.root.l.r.v)
-    print(raiz.root.l.l.v)
+    p = Stack(test)
+    
     
--- stack.py
+++ stack.py
@@ -1,7 +1,10 @@
+from tree import Node, Tree
+
 class Stack:
 
-    def __init__(self):
+    def __init__(self,test):
         self.stack = []
+        self.run(test)
     
     def pop(self):
         return self.stack.pop()
@@ -11,4 +14,23 @@ class Stack:
 
     def leitura(self):
         print(self.stack)
-        
+    
+    def run(self, test):
+        for x in test:
+            if x.isdigit():
+                new = Node(x,None,None)
+                self.push(new)
+                self.leitura()
+            else:
+                node = Node(x)
+                node.r = self.pop()
+                node.l = self.pop()
+                new = node
+                self.push(new)
+        raiz = Tree()
+        raiz.root = self.pop()
+        print(raiz.root.v)
+        print(raiz.root.r.v)
+        print(raiz.root.l.v)
+        print(raiz.root.l.r.v)
+        print(raiz.root.l.l.v)
